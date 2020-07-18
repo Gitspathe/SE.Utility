2f71cea4d94b955791fd8f9e8c41ff78fe626752
GeoSTGames <55875553+GeoSTGames@users.noreply.github.com>
2020-07-19 01:55:45 +1000

    More work on QuickParallel

--- Engine/Utility/QuickParallel.cs
+++ Engine/Utility/QuickParallel.cs
@@ -9,7 +9,7 @@ namespace SE.Utility
     {
         public static void ForEach<T>(QuickList<T> source, Action<T> action)
         {
-            int threads = Environment.ProcessorCount;
+            int threads = Math.Min(Environment.ProcessorCount - 1, 1);
             int amount = (int) Math.Floor((double) source.Count / threads);
             int curOffset = 0;
             for (int i = 0; i < threads; i++) {
@@ -19,16 +19,39 @@ namespace SE.Utility
 
                 T[] array = ArrayPool<T>.Shared.Rent(amount);
                 Array.Copy(source.Array, curOffset, array, 0, amount);
-
-                int delegateAmount = amount;
-                ThreadPool.QueueUserWorkItem(state => {
-                    for (int ii = 0; ii < delegateAmount; ii++) {
-                        action.Invoke(array[ii]);
-                    }
-                    ArrayPool<T>.Shared.Return(array);
-                });
+                QueueThread(array, amount, action);
+                
                 curOffset += amount;
             }
+        }
+
+        public static void For(int fromInclusive, int toInclusive, Action<int> body)
+        {
+            int count = toInclusive - fromInclusive;
+            int threads = Math.Min(Environment.ProcessorCount - 1, 1);
+            int amount = (int) Math.Floor((double) count / threads);
+            int curOffset = fromInclusive;
+            for (int i = 0; i < threads; i++) {
+                if (i == threads - 1) {
+                    amount = count - curOffset;
+                }
+
+                for (int ii = curOffset; ii < curOffset + amount; ii++) {
+                    body.Invoke(ii);
+                }
+
+                curOffset += amount;
+            }
+        }
+
+        private static void QueueThread<T>(T[] array, int count, Action<T> action)
+        {
+            ThreadPool.QueueUserWorkItem(state => {
+                for (int i = 0; i < count; i++) {
+                    action.Invoke(array[i]);
+                }
+                ArrayPool<T>.Shared.Return(array);
+            });
         }
     }
 }
