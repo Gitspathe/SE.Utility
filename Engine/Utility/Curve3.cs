23e323a62c5dae85bbd9fc5e9f8af7bb0b77c2bf
Gitspathe <55875553+Gitspathe@users.noreply.github.com>
2021-03-12 04:01:15 +1100

    Added Curve3

---
+++ Engine/Utility/Curve3.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+
+namespace SE.Utility
+{
+    public class Curve3
+    {
+        public Curve X = new Curve();
+        public Curve Y = new Curve();
+        public Curve Z = new Curve();
+
+        public Curve3()
+        {
+            X = new Curve();
+            Y = new Curve();
+            Z = new Curve();
+        }
+
+        public Curve3(Curve x, Curve y, Curve z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3 Evaluate(float position)
+            => new Vector3(
+                X.Evaluate(position), 
+                Y.Evaluate(position), 
+                Z.Evaluate(position));
+
+        public void Add(float position, Vector3 value)
+        {
+            X.Keys.Add(position, value.X);
+            Y.Keys.Add(position, value.Y);
+            Z.Keys.Add(position, value.Z);
+        }
+    }
+}
